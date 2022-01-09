c730f038f58de883cb984ee56c8030ca68ed3087
kelvin888 <kelvin.orhungul@gmail.com>
2022-01-09 13:07:01 +0100

    :wrench: Add Jest setup file

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -1,13 +1,25 @@
-import React from 'react'; // 1
-import { configure, shallow } from 'enzyme'; // 2
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from 'react';
+import { shallow } from 'enzyme';
 import CarouselButton from '../CarouselButton';
-
-configure({ adapter: new Adapter() });
-
-describe('CarouselButton', () => {
-  it('renders a <button>', () => {
-    const wrapper = shallow(<CarouselButton />); // 3
-    expect(wrapper.type()).toBe('button');
-  });
+describe('CarouselButton', () => { 
+    const text = 'Button text';
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+    });
+    it('renders a <button>', () => { 
+        expect(wrapper.type()).toBe('button');
+    });
+    it('passes `children` through to the <button>', () => {
+        expect(wrapper.prop('children')).toBe(text);
+    });
+    it('passes other props through to the <button>', () => {
+        const onClick = () => {};
+        const className = 'my-carousel-button';
+        const dataAction = 'prev';
+        wrapper.setProps({ onClick, className, 'data-action': dataAction });
+        expect(wrapper.prop('onClick')).toBe(onClick); 
+        expect(wrapper.prop('className')).toBe(className); 
+        expect(wrapper.prop('data-action')).toBe(dataAction);
+    }); 
 });
